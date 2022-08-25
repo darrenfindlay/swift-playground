3fe7c5a3e62fab9f742a5f80c42c48dffcdd7f21
darrenfindlay <darren@cashbackapp.com>
2022-08-25 19:58:45 +0100

    first commit

---
+++ SwiftPlayground.playground/Pages/AysncSequence.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+//: [Previous](@previous)
+
+import Foundation
+import PlaygroundSupport
+
+
+struct Value {
+	let id = UUID()
+}
+
+class ValueEmitter {
+
+	func values() -> AsyncStream<Value> {
+		
+	}
+}
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+//: [Next](@next)
